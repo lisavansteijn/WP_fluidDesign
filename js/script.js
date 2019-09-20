a2cfcf192d6f87a7142c12551d182fa951df4b54
lisavansteijn <lisavansteijn@icloud.com>
2019-09-20 14:06:44 +0200

    Navbar and footer with new folders

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// @ Lisa van Steijn
+//
+// DESC: Shows the navbar menu's fullscreen.
+
+$( "#navbarFullScreenBtn" ).click(function() {
+  $( ".nav-full-screen" ).toggle();
+  $( ".navbar-toggler-icon" ).toggleClass( "close" );
+});
