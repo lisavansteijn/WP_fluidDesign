aac8baa76b772d0842fdb62d43d3f7c804b0f9ec
lisavansteijn <lisavansteijn@icloud.com>
2019-09-25 16:22:55 +0200

    Removed titles to appear on all pages. CSS got updated for the contact page

--- index.php
+++ index.php
@@ -1,25 +1,15 @@
 <?php get_header(); ?>
   <body>
     <main role="main" class="container fd-home-container">
-      <?php if(is_user_logged_in()): ?>
       <div>
         <?php if ( have_posts() ) : while ( have_posts() ) :   the_post(); ?>
-          <h2>
-            <a href="<?php the_permalink() ?>">
-              <?php the_title(); ?>
-            </a>
-          </h2>
         <?php the_content(); ?>
         <?php endwhile; else: ?>
           <p>There no posts to show</p>
         <?php endif; ?>
       </div>
-    <?php else: ?>
-
         <div class="fd-center-div w-100">
           <h1 class="col text-center" id="countdown-text"></h1>
         </div>
-
-    <?php endif; ?>
     </main><!-- /.container -->
 <?php get_footer(); ?>
